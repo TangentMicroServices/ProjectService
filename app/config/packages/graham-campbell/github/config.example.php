d82a40be0514e403b71bf56adacb39cb53426053
Justin <justin@tangentsolutions.co.za>
2014-10-09 18:15:07 +0200

    Made a backup of github config called config.example.php and removed local config.php from repo

--- app/config/packages/graham-campbell/github/config.example.php
+++ app/config/packages/graham-campbell/github/config.example.php
@@ -24,7 +24,7 @@ return array(
     | Here you may specify which of the connections below you wish to use as
     | your default connection for all work. Of course, you may use many
     | connections at once using the manager class.
-    |
+    | 
     */
 
     'default' => 'main',
