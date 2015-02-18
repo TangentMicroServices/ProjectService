8924429756a10e440c2c2a3a155cacfb620df455
Christo Crampton <info@38.co.za>
2015-02-18 16:20:38 +0200

    update staticurl

--- projectservice/settings.py
+++ projectservice/settings.py
@@ -95,7 +95,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/srv/static/projectservice/'
+STATIC_ROOT = '/srv/static/projectservice/static/'
 
 # CUSTOM AUTH
 AUTHENTICATION_BACKENDS = (
