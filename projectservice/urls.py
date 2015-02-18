e0b561e87744e740401c2a3a2b6bb8f3245eddb9
Christo Crampton <info@38.co.za>
2015-02-18 16:09:57 +0200

    Adds api-auth, adds rest_framework.authentication.SessionAuthentication for auth to browsible API. Adds STATIC_ROOT

--- projectservice/urls.py
+++ projectservice/urls.py
@@ -6,5 +6,6 @@ from api.api import project_router
 urlpatterns = patterns('',
     # Examples:
     url(r'^api/v1/', include(project_router.urls)), 
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
 )
