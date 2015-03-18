58b217de08d5d98fc73cd52ffc71e858f5d4005c
Ian Roberts <ian@tangentsolutions.co.za>
2015-03-18 22:06:09 +0200

    Updated urls to include versioning in API

--- projectservice/urls.py
+++ projectservice/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
 from api.api import project_router
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^api/v1/', include(project_router.urls)), 
     url(r'^api-explorer/', include('rest_framework_swagger.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
