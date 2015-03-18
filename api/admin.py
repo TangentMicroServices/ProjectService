d4613140703c392ac6921c41b44f4e91dcd6b3d3
Ian Roberts <ian@tangentsolutions.co.za>
2015-03-18 13:48:49 +0200

    Added filters to admin views

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from .models import Project, Resource, Task
 
+class TaskInline(admin.TabularInline):
+	model = Task
+
+class ResourceInline(admin.TabularInline):
+	model = Resource
+
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'start_date', 'end_date', 'is_billable', 'is_active', 'created')
+	list_filter = ('is_active', 'is_billable')
+	list_display = ('title', 'description', 'start_date', 'end_date', 'is_billable', 'is_active', 'created')
+	inlines = [
+		TaskInline,
+		ResourceInline
+	]
 
 class ResourceAdmin(admin.ModelAdmin):
     list_display = ('project', 'user', 'start_date', 'end_date', 'rate', 'agreed_hours_per_month', 'created')
