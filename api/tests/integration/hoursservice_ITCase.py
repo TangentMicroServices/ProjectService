0d3375aa5a76635b9ff31abf894951de65a00120
Ian Roberts <ian@tangentsolutions.co.za>
2015-03-30 13:40:33 +0200

    Updated integration tests on project

--- api/tests/integration/hoursservice_ITCase.py
+++ api/tests/integration/hoursservice_ITCase.py
@@ -16,16 +16,3 @@ def mock_auth_success(user=None):
 		response_string = json.dumps(response_string)
 	
 	responses.add(responses.GET, url, body=response_string, status=200, content_type='application/json')
-
-class HoursServiceTestCase(TestCase):
-
-	def test_get_entries(self):
-		
-		mock_auth_success()
-		url = '{0}/api/v1/entry/' . format(settings.HOURSSERVICE_BASE_URL)
-
-		response = self.client.get(url, content_type='application/json')
-		
-		assert response.status_code == 200, 'Expect 200 OK'
-		#assert response.json().get("username") == "admin", 'Expect the current user to be returned'
-
